2b0a3890919f8d3771c60e8ec6eeab69b7d55166
Dagmawi Solomon Tilahun <dagisolo1023@gmail.com>
2026-05-09 21:14:33 +0300

    added / route to proxy for checking

--- web/src/proxy.ts
+++ web/src/proxy.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/forgot-password(.*)", "/verification(.*)"]);
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)", "/forgot-password(.*)", "/verification(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
     if (!isPublicRoute(req)) await auth.protect();
