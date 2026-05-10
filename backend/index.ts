2036bece37f950e3d73e4abec9d756b0a1f1fa6f
Dagmawi Solomon Tilahun <dagisolo1023@gmail.com>
2026-05-10 08:55:46 +0300

    Backend server prisma connection confirmation and more ENVs

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,8 @@
 import app from "@/lib/app";
+import ENV from "@/util/env";
+import prisma from "@/lib/prisma";
 import { createServer } from "http";
 import initializeSocket from "@/lib/socket";
-
-const port = process.env.PORT;
 
 const server = createServer(app);
 
@@ -10,4 +10,20 @@ const io = initializeSocket(server);
 
 app.set("io", io);
 
-server.listen(port, () => console.log(`🚀 Server running at http://localhost:${port}`));
+async function start() {
+    try {
+        await prisma.$connect();
+        console.log("✅ Database connected");
+
+        const port = ENV.PORT;
+        const serverUrl = ENV.NODE_ENV === "development" ? ENV.LOCAL_URL + port : ENV.SERVER_URL;
+        console.log("DATABASE_URL exists:", ENV.DATABASE_URL);
+
+        server.listen(port, () => console.log("🚀 Server running on", serverUrl));
+    } catch (err) {
+        console.error("❌ Database failed", err);
+        process.exit(1);
+    }
+}
+
+start();
--- backend/src/util/env.ts
+++ backend/src/util/env.ts
@@ -8,7 +8,10 @@ function requireEnv(key: string): string {
 
 const ENV = {
     PORT: requireEnv("PORT"),
+    NODE_ENV: requireEnv("NODE_ENV") as "production" | "development",
+    LOCAL_URL: requireEnv("LOCAL_URL"),
     CLIENT_URL: requireEnv("CLIENT_URL"),
+    SERVER_URL: requireEnv("SERVER_URL"),
     DATABASE_URL: requireEnv("DATABASE_URL"),
     CLERK_SECRET_KEY: requireEnv("CLERK_SECRET_KEY"),
     CLERK_PUBLISHABLE_KEY: requireEnv("CLERK_PUBLISHABLE_KEY"),
